e721c0835efc71ebd1c8a81be3c06b26dc64d05c
Yamkela Zungula <zungulay@gmail.com>
2023-03-17 14:44:22 +0200

    reactive and ref behaviour

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+ import Reactivity from './components/Reactivity.vue';
 </script>
 
 <template>
   <div>
     <h1>Vue playground</h1>
+    <Reactivity />
   </div>
 </template>
 
---
+++ src/components/Reactivity.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+     <h1>Reactive count: {{ state.count }}</h1> 
+     <h1>Ref count: {{ count }}</h1>
+     <button @click="increment">Increment</button>
+  </div>
+</template>
+<script>
+import { reactive, ref } from 'vue';
+export default {
+  setup() {
+    const state = reactive({ count: 1 });
+    const count = ref(0);
+
+    function increment() {
+      state.count++;
+      count.value++;
+    }
+    return {
+      state,
+      count,
+      increment
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
