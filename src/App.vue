ed6a074919f00255bb3bd9308711be8792395c86
Yamkela Zungula <zungulay@gmail.com>
2023-04-04 20:09:58 +0200

    Template refs example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script setup>
-</script>
-
 <template>
   <div>
-    <h1>Vue playground</h1>
+    <h1>Template Refs</h1>
+    <input type="text" ref="inputRef">
+    <ul>
+      <li v-for="(item, index) in list" :key="index" ref="itemRefs">{{ item }}</li>
+    </ul>
   </div>
 </template>
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const inputRef = ref(null)
+const itemRefs = ref([])
+const list = ref([1, 2, 3, 4, 5])
+
+onMounted(() => {
+  inputRef.value.focus();
+  console.log(itemRefs.value);
+})
+</script>
 
 <style scoped>
-
 </style>
