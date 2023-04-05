01cdbf4aa5ea106851320638e8b9eea3198c6e8e
Yamkela Zungula <zungulay@gmail.com>
2023-04-05 12:04:25 +0200

    Fallthrough attributes example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import BaseButton from "./components/BaseButton.vue";
+const btnClicked = () => {
+  console.log("Button clicked");
+}
 </script>
 
 <template>
   <div>
     <h1 class="text-green-500 text-2xl text-center m-5">Vue playground</h1>
+    <BaseButton @click="btnClicked" class="bg-green-400 rounded-md p-2 text-xl m-2"/>
   </div>
 </template>
 
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button>Click</button>
+</template>
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
