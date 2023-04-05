004b4591cd615eb34b99d3e47ac4e632b7f92de4
Yamkela Zungula <zungulay@gmail.com>
2023-04-05 18:50:42 +0200

    named slots

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BaseLayout from "./components/BaseLayout.vue";
 import FancyButton from "./components/FancyButton.vue";
 </script>
 
@@ -6,6 +7,17 @@ import FancyButton from "./components/FancyButton.vue";
   <div class="bg-zinc-800 h-full w-full">
     <h1 class="text-green-500 text-2xl text-center m-5">Vue playground</h1>
     <FancyButton class="bg-sky-500 rounded-md p-2">Click</FancyButton>
+    <BaseLayout>
+      <template #header>
+        <h1>Header Slot</h1>
+      </template>
+      <template #default>
+         <p>Main slot</p>
+      </template>
+      <template #footer>
+        <p>Footer slot</p>
+      </template>
+    </BaseLayout>
   </div>
 </template>
 
