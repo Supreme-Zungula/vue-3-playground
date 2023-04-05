7c0a38c0e1b2abb715e53ab1f765872e9df22031
Yamkela Zungula <zungulay@gmail.com>
2023-04-05 19:37:01 +0200

    Scoped slots example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
-<script setup>
-import BaseLayout from "./components/BaseLayout.vue";
-import FancyButton from "./components/FancyButton.vue";
-</script>
-
 <template>
   <div class="bg-zinc-800 h-full w-full">
     <h1 class="text-green-500 text-2xl text-center m-5">Vue playground</h1>
+    <!-- Basic slot -->
     <FancyButton class="bg-sky-500 rounded-md p-2">Click</FancyButton>
+    <!-- Named slot -->
     <BaseLayout>
       <template #header>
         <h1>Header Slot</h1>
@@ -18,8 +15,29 @@ import FancyButton from "./components/FancyButton.vue";
         <p>Footer slot</p>
       </template>
     </BaseLayout>
+    <!-- Scoped slot -->
+    <FancyList :list="usersList">
+     <template #item="{ name, surname }">
+      <span class="bg-sky-500 rounded-md text-lime-500">{{ name }} {{ surname }}</span>
+     </template>
+    </FancyList>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import BaseLayout from "./components/BaseLayout.vue";
+import FancyButton from "./components/FancyButton.vue";
+import FancyList from "./components/FancyList.vue";
+
+const usersList = ref([
+  { name: "Yamkela", surname: "Zungula" },
+  { name: "Buhle", surname: "Zungula" },
+  { name: "Litha", surname: "Zungula" },
+  { name: "Athi", surname: "Zungula" },
+  { name: "Siyolo", surname: "Zungula" },
+])
+</script>
 
 <style scoped>
 
---
+++ src/components/FancyList.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 v-if="list.length === 0">List empty</h2>
+  <ul v-else>
+    <li v-for="(item, index) in list" :key="index">
+      <slot name="item" v-bind="item"></slot>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      default: []
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
